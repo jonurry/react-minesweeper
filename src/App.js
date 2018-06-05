25e53e4a7d4ffd2f8fc8e59d735759bf471f5342
Jon Urry <github@agileweb.co.uk>
2018-06-05 15:12:37 +0100

    Added toolbar, controls and theme.

--- src/App.js
+++ src/App.js
@@ -1,21 +1,108 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
 import './App.css';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+import Button from '@material-ui/core/Button';
+import Badge from '@material-ui/core/Badge';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import { createMuiTheme } from '@material-ui/core/styles';
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#9be7ff',
+      main: '#64b5f6',
+      dark: '#2286c3',
+      contrastText: '#263238'
+    },
+    secondary: {
+      light: '#ffe97d',
+      main: '#ffb74d',
+      dark: '#c88719',
+      contrastText: '#263238'
+    }
+  }
+});
+
+const styles = theme => ({
+  margin: {
+    margin: theme.spacing.unit * 2
+  },
+  padding: {
+    padding: `0 ${theme.spacing.unit * 2}px`
+  }
+});
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      difficulty: 1
+    };
+    this.classes = props.classes;
+  }
+  handleChangeDifficulty = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <MuiThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="App">
+          <AppBar position="static">
+            <Toolbar>
+              <Select
+                className={this.classes.margin}
+                color=""
+                value={this.state.difficulty}
+                onChange={this.handleChangeDifficulty}
+                inputProps={{
+                  name: 'difficulty',
+                  id: 'select-difficulty'
+                }}
+              >
+                <MenuItem value={1}>Easy</MenuItem>
+                <MenuItem value={2}>Medium</MenuItem>
+                <MenuItem value={3}>Hard</MenuItem>
+              </Select>
+              <Button variant="fab" color="secondary" aria-label="go">
+                Go
+              </Button>
+              <Typography
+                className={this.classes.margin}
+                variant="subheading"
+                color="inherit"
+              >
+                <Badge
+                  className={this.classes.padding}
+                  badgeContent={4}
+                  color="secondary"
+                >
+                  Mines
+                </Badge>
+              </Typography>
+              <Typography
+                className={this.classes.margin}
+                variant="subheading"
+                color="inherit"
+              >
+                00:43
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </div>
+      </MuiThemeProvider>
     );
   }
 }
 
-export default App;
+App.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(App);
