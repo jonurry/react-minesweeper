32e43618c4ee6a4374fafe8016a5eb786ea830d3
Jon Urry <github@agileweb.co.uk>
2018-06-20 10:30:07 +0100

    Refactored Minefield to separate file.

--- src/App.js
+++ src/App.js
@@ -13,82 +13,9 @@ import Badge from '@material-ui/core/Badge';
 import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
 import { theme, styles, styleToolbarCentre } from './theme.js';
 import Timer from './timer.js';
-import { FlagIcon, MineIcon } from './icons.js';
 import Emoji from './emoji.js';
+import Minefield from './minefield.js';
 import Model, { FLAGS, GAME_STATUS } from './model.js';
-
-const Minefield = props => {
-  let minefield = [];
-  let key = 0;
-  let fontSize =
-    typeof props.minefieldStyle === 'undefined'
-      ? '100%'
-      : props.minefieldStyle.fontSize;
-  for (let item of props.minefield) {
-    let content = '';
-    if (item.revealed) {
-      switch (item.value) {
-        case 0:
-          content = '';
-          break;
-        case '*':
-          content = (
-            <MineIcon
-              className={`${props.classes.icon} ${
-                props.trippedMineId === item.id ? 'pulse' : ''
-              }`}
-              style={{
-                fontSize: fontSize,
-                color:
-                  props.trippedMineId === item.id
-                    ? theme.palette.state.bad
-                    : theme.palette.secondary.main
-              }}
-            />
-          );
-          break;
-
-        default:
-          content = item.value;
-          break;
-      }
-    }
-    let flag;
-    switch (item.flag) {
-      case FLAGS.mine:
-        flag = (
-          <FlagIcon
-            className={props.classes.icon}
-            style={{
-              fontSize: fontSize
-            }}
-          />
-        );
-        break;
-      case FLAGS.possible:
-        flag = '?';
-        break;
-      case FLAGS.none:
-      default:
-        flag = '';
-        break;
-    }
-    minefield.push(
-      <div className="scene" key={key}>
-        <div
-          className={`card ${item.revealed ? 'reveal' : ''}`}
-          onClick={props.handleClick.bind(this, key)}
-          onContextMenu={props.handleClick.bind(this, key)}
-        >
-          <div className="card-face front">{flag}</div>
-          <div className="card-face back">{content}</div>
-        </div>
-      </div>
-    );
-    key++;
-  }
-  return minefield;
-};
 
 class App extends Component {
   constructor(props) {
