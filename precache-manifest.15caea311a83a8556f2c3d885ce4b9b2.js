91cea338232310c2ff5e86295bb0134be1489fec
Jon Urry <jon@urry.me>
2019-01-30 11:21:45 +0000

    Updates

---
+++ precache-manifest.15caea311a83a8556f2c3d885ce4b9b2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c80af3cb660a53b4a2e",
+    "url": "/react-minesweeper/static/js/runtime~main.1c80af3c.js"
+  },
+  {
+    "revision": "01e00356be40291db5b5",
+    "url": "/react-minesweeper/static/js/main.01e00356.chunk.js"
+  },
+  {
+    "revision": "43c7fa5f0c9858f2173d",
+    "url": "/react-minesweeper/static/js/1.43c7fa5f.chunk.js"
+  },
+  {
+    "revision": "01e00356be40291db5b5",
+    "url": "/react-minesweeper/static/css/main.7d1cc42d.chunk.css"
+  },
+  {
+    "revision": "3535b5968a281a17fed064b91f24d687",
+    "url": "/react-minesweeper/index.html"
+  }
+];
